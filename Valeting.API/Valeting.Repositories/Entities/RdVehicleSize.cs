81a95b04f8ade342cf9f8ecbfdfafccf90fc0d83
tmb37 <tmntb.work@gmail.com>
2024-03-12 20:50:32 +0000

    Remove unnecessary usings

--- Valeting.API/Valeting.Repositories/Entities/RdVehicleSize.cs
+++ Valeting.API/Valeting.Repositories/Entities/RdVehicleSize.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Valeting.Repositories.Entities
+﻿namespace Valeting.Repositories.Entities
 {
     public partial class RdVehicleSize
     {
