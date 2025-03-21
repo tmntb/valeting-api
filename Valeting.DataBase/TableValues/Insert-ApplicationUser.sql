df4a57cdac1202f88ab5b9235154a9bc097f6572
tmb37 <tmntb.work@gmail.com>
2025-03-21 14:52:56 +0000

    remove unnecessary directory

--- Valeting.DataBase/TableValues/Insert-ApplicationUser.sql
+++
@@ -1,4 +0,0 @@
-﻿USE Valeting
-
-insert into ApplicationUser (id, Username, Password, Salt)
-values(newid(), 'admin@gmail.com', 'C1p5x44JSbJ9Ln6wCv97g3fSERYR207D2TCya1aJKks=', 'g????r??~=?_???') --admin37
--- Valeting.DataBase/TableValues/Insert-RD_Flexibility.sql
+++
@@ -1,8 +0,0 @@
-USE Valeting
-
-INSERT INTO RD_Flexibility (ID, [Description], Active)
-VALUES (NEWID(), '+/- 1 Day', 1)
-INSERT INTO RD_Flexibility (ID, [Description], Active)
-VALUES (NEWID(), '+/- 2 Days', 1)
-INSERT INTO RD_Flexibility (ID, [Description], Active)
-VALUES (NEWID(), '+/- 3 Days', 1)
