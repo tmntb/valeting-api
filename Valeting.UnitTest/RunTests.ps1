fb50a15208297970d680eb575d4ceaf2a7a88c5b
tmb37 <tmntb.work@gmail.com>
2025-03-21 14:53:06 +0000

    update files

--- Valeting.UnitTest/RunTests.ps1
+++ Valeting.UnitTest/RunTests.ps1
@@ -1,10 +1,5 @@
 ﻿dotnet test --collect:"XPlat Code Coverage"
 
-$coverageFiles = Get-ChildItem -Path "./TestResults" -Recurse -Filter "coverage.cobertura.xml"
-$latestCoverageFile = $coverageFiles | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+$latestDir = Get-ChildItem -Path .\TestResults\ -Directory | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
-if ($null -eq $latestCoverageFile) {
-    exit 1
-}
-
-reportgenerator -reports:$latestCoverageFile.FullName -targetdir:./TestResults -reporttypes:Html
+reportgenerator -reports:"($latestDir.FullName)\coverage.cobertura.xml" -targetdir:.\TestResults\coveragereport
